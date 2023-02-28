2a1a59db8947abc54a947c4334e35271e08119ed
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-02-28 22:49:47 +0200

    push profile user

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+export const Profile = ({
+    avatar,
+     username,
+      tag, 
+      location, 
+      stats,
+
+}) =>  (
+    <div class={css.profile}>
+  <div class="description">
+    <img
+      src={avatar}
+      alt="User avatar"
+      class="avatar"
+    />
+    <p class="name">{username}</p>
+    <p class="tag">{tag}</p>
+    <p class="location">{location}</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">{stats.followers}</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">{stats.view}</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+);
+Profile.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    stats: PropTypes.object.isRequired,
+  };
